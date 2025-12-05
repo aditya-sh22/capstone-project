23255ee26080df97e47cba007d4eae046abe0230
aditya-sh22 <aditya03092@outlook.com>
2025-12-05 19:07:16 +0530

    Added files

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template, redirect
+import os, pymysql
+from dotenv import load_dotenv
+load_dotenv()
+
+app=Flask(__name__)
+
+db_conn = pymysql.connect(
+    host=os.getenv("DBHOST"),
+    port=int(os.getenv("DBPORT")),
+    user=os.getenv("DBUSER"),
+    password=os.getenv("DBPWD"),
+    database=os.getenv("DATABASE")
+)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name=request.form['name']
+    email=request.form['email']
+    role=request.form['role']
+    cur=db_conn.cursor()
+    cur.execute("INSERT INTO employees (name,email,role) VALUES (%s,%s,%s)",(name,email,role))
+    db_conn.commit()
+    return redirect('/')
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
